9492ba0cd65ee585befaf9e7bde02726de3e255e
VinceGonzales <vince.gonzales@hotmail.com>
2018-09-25 15:34:11 -0700

    neat trick to copy html files

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@
 import Service = require('./server/WebService');
 
 var webService: Service.WebService = new Service.WebService();
+webService.copyDirContent("../../src/web/");
 webService.start(8081);
--- src/server/WebService.ts
+++ src/server/WebService.ts
@@ -33,4 +33,22 @@ export class WebService {
 			this.response.end();
 		}
 	}
+
+	public copyDirContent(startPath:string): void {
+		var filepath: string = path.join(__dirname, startPath);
+		if (!fs.existsSync(filepath)) {
+			console.log("no dir ", startPath);
+			return;
+		}
+		fs.readdir(filepath, (err:NodeJS.ErrnoException, files: string[]): void => {
+			for (var file of files) {
+				var a: string = path.join(filepath, file);
+				var b: string = path.join(filepath, "../../dist/" + file);
+				var source: fs.ReadStream = fs.createReadStream(a);
+				var dest: fs.WriteStream = fs.createWriteStream(b);
+				source.pipe(dest);
+				console.log(`copied file from ${a} to ${b}`);
+			}
+		});
+	}
 }
