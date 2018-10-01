fe3aacd978cd1a6d0615cd7a5092952c93b3c894
VinceGonzales <vince.gonzales@hotmail.com>
2018-10-01 09:51:50 -0700

    created a file manager

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 "use strict"
 import Service = require('./server/WebService');
+import FileMgr = require('./server/Filemanager');
 
 var webService: Service.WebService = new Service.WebService();
-webService.copyDirContent("../../src/web/");
+var fileManager: FileMgr.FileManager = new FileMgr.FileManager();
+if (!fileManager.checkFile('index.html')) {
+	console.log('Copying files!');
+	fileManager.copyDirContent("../../src/web/");
+}
 webService.start(8081);
